7df4229819c2faa4871ed094d874112617788bee
Damian Krzeminski <pirxpilot@code42day.com>
2015-05-13 20:03:52 -0400

    implemenation and tests

--- test/kml.js
+++ test/kml.js
@@ -1,9 +1,26 @@
-var should = require('should');
-var furkotKml = require('../');
+var fs = require('fs');
+var path = require('path');
+var kml = require('../');
+
+function readFileSync(name) {
+  return fs.readFileSync(path.join(__dirname, name), 'utf8');
+}
 
 describe('furkot-kml node module', function () {
-  it('must have at least one test', function () {
-    furkotKml();
-    should.fail('Need to write tests.');
+  it('simple trip', function() {
+    var t = require('./fixtures/simple-trip.json'),
+      generated = kml(t),
+      expected = readFileSync('fixtures/simple.kml');
+
+    generated.should.eql(expected);
+  });
+
+  it('multi trip', function() {
+    var t = require('./fixtures/multi-trip.json'),
+      generated = kml(t),
+      expected = readFileSync('fixtures/multi.kml');
+
+    generated.should.eql(expected);
+
   });
 });
